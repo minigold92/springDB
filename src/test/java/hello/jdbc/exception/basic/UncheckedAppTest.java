d13638cd88a09fb1f470d158b6503969baf68902
eddie <minigold92@gmail.com>
2023-08-19 21:25:18 +0900

    런타임예외는 대부분 복구 불가능한 예외라서, 서비스나 컨트롤러가 신경쓰지 않아도 됨. 일관성있게 공통으로 처리하면 된다. 런타임예외는 해당 객체가 처리할 수 없다면 무시하면 된다. 따라서 예외에 의존하지 않게된다. 사용하는 기술이 변하더라도, 예외를 공통으로 처리하는 부분만 수정하면 된다. 체크예외를 언체크예외로 전환할 때, 기존예외를 cause로 포함.

--- src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -1,16 +1,30 @@
 package hello.jdbc.exception.basic;
 
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+@Slf4j
 public class UncheckedAppTest {
 
     @Test
     void unchecked() {
         Controller controller = new Controller();
         Assertions.assertThatThrownBy(controller::request).isInstanceOf(Exception.class);
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+//            e.printStackTrace();//이렇게 해도 되지만 좋지 않음.system.out으로 출력되는 것.
+            log.info("ex", e);
+        }
     }
 
     static class Controller {
@@ -44,6 +58,7 @@ public class UncheckedAppTest {
                 runSQL();
             } catch (SQLException e) {
                 //예외를 던질 때 기존 예외를 넣어줘야함. 그래야, 상위 예외에서 발생한 stackTrace도 확인할 수 있음.
+//                throw new RuntimeSQLException();//기존 e를 cause로 넣어주지 않으면 무엇때문에 터지는지 알 수 없음.
                 throw new RuntimeSQLException(e);
             }
         }
@@ -59,6 +74,7 @@ public class UncheckedAppTest {
         }
     }
 
+    @NoArgsConstructor
     static class RuntimeSQLException extends RuntimeException {
         /**
          * cause를 넣어줘야 상위 예외의 스택트레이스 확인 가능.
