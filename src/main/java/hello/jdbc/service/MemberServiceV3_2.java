524226f7772380ffdd6e2fc67f80439f4ef5533e
eddie <minigold92@gmail.com>
2023-08-19 18:06:55 +0900

    스프링 트랜잭션 AOP를 통한 트랜잭션 코드와 비즈니스 코드 분리 및 autoconfiguration

--- src/main/java/hello/jdbc/service/MemberServiceV3_2.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -2,14 +2,10 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionTemplate;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
@@ -42,17 +38,6 @@ public class MemberServiceV3_2 {
         memberRepository.update(fromId, fromMember.getMoney() - money);
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
-    }
-
-    private static void release(Connection con) {
-        if (con != null){
-            try {
-                con.setAutoCommit(true);//커넥션 풀을 사용하기에 true로 반환해야함.
-                con.close();
-            }catch (Exception e){
-                log.info("error", e);
-            }
-        }
     }
 
     private void validation(Member toMember) {
--- src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -4,34 +4,24 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랜잭션 매니저
+ * 트랜잭션 - @Transactional AOP
  */
 @Slf4j
 @RequiredArgsConstructor
-public class MemberServiceV3_1 {
-    private final PlatformTransactionManager transactionManager;
+public class MemberServiceV3_3 {
     private final MemberRepositoryV3 memberRepository;
 
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        TransactionStatus  status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        try {
-            //비즈니스 로직
-            bizLogic(fromId, toId, money);
-            transactionManager.commit(status);
-        } catch (Exception e) {
-            transactionManager.rollback(status);
-            throw new IllegalStateException(e);
-        }
+        bizLogic(fromId, toId, money);
     }
 
-    private void bizLogic( String fromId, String toId, int money) throws SQLException {
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
         memberRepository.update(fromId, fromMember.getMoney() - money);
